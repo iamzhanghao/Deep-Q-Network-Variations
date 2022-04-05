b04d9165cd9edf1f507c21342e7df4d1297136fe
Zhang Hao <zhangghaoo@gmail.com>
2022-04-05 23:37:23 +0800

    Add ConvDuelingDQN class

--- model.py
+++ model.py
@@ -7,6 +7,11 @@ import numpy as np
 import math, random
 
 
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform(m.weight)
+        m.bias.data.fill_(0.01)
+
 class CnnDQN(nn.Module):
     def __init__(self,  num_actions):
         super(CnnDQN, self).__init__()
@@ -29,6 +34,9 @@ class CnnDQN(nn.Module):
             nn.Linear(512, self.num_actions)
         )
         
+        self.features.apply(init_weights)
+        self.fc.apply(init_weights)
+        
     def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), -1)
@@ -49,9 +57,62 @@ class CnnDQN(nn.Module):
         return action
 
     def eval(self, state):
-        state = torch.FloatTensor(state).unsqueeze(0)
-        state = state.cuda()
-        q_value = self.forward(state)
-        action  = q_value.max(1)[1].data[0]
+        action = self.act(state,0)
    
         return action
+
+
+
+class ConvDuelingDQN(nn.Module):
+
+    def __init__(self, input_dim, output_dim):
+        super(ConvDuelingDQN, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.fc_input_dim = self.feature_size()
+        
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_dim[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.value_stream = nn.Sequential(
+            nn.Linear(self.fc_input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+        self.advantage_stream = nn.Sequential(
+            nn.Linear(self.fc_input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, self.output_dim)
+        )
+
+    def forward(self, state):
+        features = self.conv(state)
+        features = features.view(features.size(0), -1)
+        values = self.value_stream(features)
+        advantages = self.advantage_stream(features)
+        qvals = values + (advantages - advantages.mean())
+        
+        return qvals
+    
+    
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            action = self.forward(state)
+        else:
+            action = random.randrange(self.num_actions)
+        return action
+
+    def eval(self, state):
+        action = self.act(state,0)
+   
+        return action
+
+    def feature_size(self):
+        return self.conv(autograd.Variable(torch.zeros(1, *self.input_dim))).view(1, -1).size(1)
